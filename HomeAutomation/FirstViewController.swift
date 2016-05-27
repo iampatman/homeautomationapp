579220966672d50ea4f78a9394e0f4ee2ba13d07
Nguyen Trung <nguyentrung0904@gmail.com>
2016-05-27 10:51:38 +0800

    Initial Commit

---
+++ HomeAutomation/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HomeAutomation
+//
+//  Created by Nguyen Bui An Trung on 27/5/16.
+//  Copyright © 2016 Nguyen Bui An Trung. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
