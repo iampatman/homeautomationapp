008b6d4fa9befacdf7a12b85e0214b4631165416
Nguyen Trung <nguyentrung0904@gmail.com>
2016-06-05 11:53:08 +0800

    hello

---
+++ HomeAutomation-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  HomeAutomation-Bridging-Header.h
+//  HomeAutomation
+//
+//  Created by Nguyen Bui An Trung on 3/6/16.
+//  Copyright © 2016 Nguyen Bui An Trung. All rights reserved.
+//
+
+#ifndef HomeAutomation_Bridging_Header_h
+#define HomeAutomation_Bridging_Header_h
+
+
+#endif /* HomeAutomation_Bridging_Header_h */
---
+++ HomeAutomation/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  HomeAutomation
+//
+//  Created by Nguyen Bui An Trung on 3/6/16.
+//  Copyright © 2016 Nguyen Bui An Trung. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: UIViewController {
+
+}
